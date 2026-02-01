8aeca73bc054056c0bab1888e6b7f3693d914828
Pham Tran Duong <duong0979826014@gmail.com>
2026-02-01 15:12:59 +0700

    sửa lại cấu trúc code dự án

---
+++ src/all_header.h
@@ -0,0 +1,14 @@
+#ifndef __ALL_HEADER_H__
+#define __ALL_HEADER_H__
+
+#include <Arduino.h>
+#include <stdlib.h>
+
+#include "BMP180.h"
+#include "DHT11.h"
+#include "GPS.h"
+#include "GSM.h"
+#include "Rainfall.h"
+#include "WindSpeed.h"
+
+#endif // __ALL_HEADER_H__
